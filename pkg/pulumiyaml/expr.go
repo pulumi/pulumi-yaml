c4b64ffd923b3104698b9e2a94d53bf493477022
Aaron Friel <mayreply@aaronfriel.com>
2022-02-03 17:20:14 -0800

    fix: null pointer deref due to constructing symbolexpr without syntax node

--- pkg/pulumiyaml/expr.go
+++ pkg/pulumiyaml/expr.go
@@ -42,7 +42,9 @@ func getExpressionDependencies(deps *[]*ast.StringExpr, x ast.Expr) {
 	case *ast.InterpolateExpr:
 		for _, p := range x.Parts {
 			if p.Value != nil {
-				getExpressionDependencies(deps, &ast.SymbolExpr{Property: p.Value})
+				name := p.Value.Accessors[0].(*ast.PropertyName).Name
+				sx := ast.StringSyntax(syntax.StringSyntax(x.Syntax().Syntax(), name))
+				*deps = append(*deps, sx)
 			}
 		}
 	case *ast.SymbolExpr:
