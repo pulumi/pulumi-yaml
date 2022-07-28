36cb0525d9bb612ef3c0596871b80e8da2118ec7
Alex Qiu <aqiu@pulumi.com>
2022-07-28 09:23:35 -0700

    Update pkg/pulumiyaml/ast/expr.go
    Co-authored-by: Ian Wahbe <ian@wahbe.com>

--- pkg/pulumiyaml/ast/expr.go
+++ pkg/pulumiyaml/ast/expr.go
@@ -12,7 +12,7 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 )
 
-const fnInvokeRegexPattern = "Fn::.+:.+(:.+)?"
+var fnInvokeRegex = regex.MustCompile("Fn::.+:.+(:.+)?")
 
 // Expr represents a Pulumi YAML expression. Expressions may be literals, interpolated strings, symbols, or builtin
 // functions.
