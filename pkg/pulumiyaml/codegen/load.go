a0907b4d406c59bfa90eff4e2480e6ca454bb04e
aq17 <aqiu@pulumi.com>
2022-09-12 17:16:39 -0700

    choose latest pkg over set provider

--- pkg/pulumiyaml/codegen/load.go
+++ pkg/pulumiyaml/codegen/load.go
@@ -771,6 +771,7 @@ func (imp *importer) importResource(kvp ast.ResourcesMapEntry, latestPkgInfo map
 			Value: &model.LiteralValueExpression{Value: cty.BoolVal(resource.Options.Protect.Value)},
 		})
 	}
+	// latest package settings takes precedence over a set provider
 	if p, ok := latestPkgInfo[pkg.Name()]; ok {
 		if p.version != "" {
 			resourceOptions.Body.Items = append(resourceOptions.Body.Items, &model.Attribute{
@@ -784,9 +785,7 @@ func (imp *importer) importResource(kvp ast.ResourcesMapEntry, latestPkgInfo map
 				Value: quotedLit(p.pluginDownloadURL),
 			})
 		}
-
-	}
-	if resource.Options.Provider != nil {
+	} else if resource.Options.Provider != nil {
 		ref, err := imp.getResourceRefItem(resource.Options.Provider, name, "provider")
 		if err != nil {
 			diags.Extend(err)
