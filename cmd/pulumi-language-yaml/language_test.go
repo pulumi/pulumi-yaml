7fa6019565c9cbea070f921d636fd3b145e7f9c9
Ian Wahbe <me@iwahbe.com>
2026-02-02 19:06:10 +0100

    Uncomment l1-output-null

--- cmd/pulumi-language-yaml/language_test.go
+++ cmd/pulumi-language-yaml/language_test.go
@@ -178,7 +178,6 @@ var expectedFailures = map[string]string{
 	"l1-builtin-stash":                             "not yet implemented",
 	"l1-builtin-try":                               "#721 generation unimplemented",
 	"l1-config-types":                              "Failed to generate YAML program: *model.BinaryOpExpression; Unimplemented! Needed for  aNumber + 1.25",
-	"l1-output-null":                               "test failing",
 	"l1-proxy-index":                               "run bailed",
 	"l2-component-call-simple":                     "#722 generation unimplemented",
 	"l2-component-property-deps":                   "Traversal not allowed on function result",
