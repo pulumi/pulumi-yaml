2796032a9069f6ec0a68acd297e87c156257e739
Ian Wahbe <me@iwahbe.com>
2026-03-17 16:38:44 +0100

    Fix `l1-output-string`
    The actual fix is https://github.com/pulumi/pulumi/pull/22247. This PR just
    consumes it by updating the version of pulumi/pulumi used.

--- cmd/pulumi-language-yaml/testdata/eject-pcl/l2-keywords/program.pp
+++ cmd/pulumi-language-yaml/testdata/eject-pcl/l2-keywords/program.pp
@@ -1,11 +1,27 @@
 resource firstResource "keywords:index:SomeResource" {
 	__logicalName = "firstResource"
 	builtins = "builtins"
+	lambda = "lambda"
 	property = "property"
 }
 
 resource secondResource "keywords:index:SomeResource" {
 	__logicalName = "secondResource"
 	builtins = firstResource.builtins
+	lambda = firstResource.lambda
 	property = firstResource.property
+}
+
+resource lambdaModuleResource "keywords:lambda:SomeResource" {
+	__logicalName = "lambdaModuleResource"
+	builtins = "builtins"
+	lambda = "lambda"
+	property = "property"
+}
+
+resource lambdaResource "keywords:index:Lambda" {
+	__logicalName = "lambdaResource"
+	builtins = "builtins"
+	lambda = "lambda"
+	property = "property"
 }
--- go.mod
+++ go.mod
@@ -12,8 +12,8 @@ require (
 	github.com/hexops/autogold v1.3.0
 	github.com/iancoleman/strcase v0.2.0
 	github.com/pkg/errors v0.9.1
-	github.com/pulumi/pulumi/pkg/v3 v3.226.0
-	github.com/pulumi/pulumi/sdk/v3 v3.226.0
+	github.com/pulumi/pulumi/pkg/v3 v3.226.1-0.20260317165701-8e516dbc7dd9
+	github.com/pulumi/pulumi/sdk/v3 v3.226.1-0.20260317165701-8e516dbc7dd9
 	github.com/spf13/afero v1.9.5
 	github.com/stretchr/testify v1.11.1
 	github.com/zclconf/go-cty v1.13.2
