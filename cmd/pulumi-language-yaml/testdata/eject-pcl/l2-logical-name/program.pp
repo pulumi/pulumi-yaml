bf78c72a621b0176b388e432287a9a1688bb7115
Ian Wahbe <me@iwahbe.com>
2026-03-19 14:10:52 +0100

    Fix l2-logical-name eject round-trip
    When ejecting YAML to PCL, the importer was using the YAML map key as the
    `__logicalName` for config variables and resources. It should use the YAML
    `name:` property instead, since that is the actual logical name used in
    Pulumi state. The map key is just the lexical identifier within the YAML
    program.

--- cmd/pulumi-language-yaml/testdata/eject-pcl/l2-logical-name/program.pp
+++ cmd/pulumi-language-yaml/testdata/eject-pcl/l2-logical-name/program.pp
@@ -1,18 +1,18 @@
 config configLexicalName bool {
-	__logicalName = "configLexicalName"
+	__logicalName = "cC-Charlie_charlie.\U0001f603⁉️"
 }
 
 resource resourceLexicalName "simple:index:Resource" {
-	__logicalName = "resourceLexicalName"
+	__logicalName = "aA-Alpha_alpha.\U0001f92f⁉️"
 	value = configLexicalName
 }
 
 output bBBetaBeta {
-	__logicalName = "bB-Beta_beta.💜⁉"
+	__logicalName = "bB-Beta_beta.\U0001f49c⁉"
 	value = resourceLexicalName.value
 }
 
 output dDDeltaDelta {
-	__logicalName = "dD-Delta_delta.🔥⁉"
+	__logicalName = "dD-Delta_delta.\U0001f525⁉"
 	value = resourceLexicalName.value
 }
